194115a2bd1de45c6232f1af2e78cedfaeadfb73
NidhitheCoder <nidhpv@gmail.com>
2020-12-02 11:15:43 +0530

    Updating with reducer

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,19 +1,24 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 
 const BlogContext = React.createContext();
+const blogReducer = (state,action) =>{
+  switch(action.type) {
+    case 'add_blogPost':
+      return [...state,{title:`Blogpost #${state.length + 1}`}];
+    default:
+      return state;
+  }
+}
 
 export const BlogProvider = ({ children }) => {
-  const [blogPosts, setBlogPosts] = useState([]);
+  const [blogPosts, dispatch] = useReducer(blogReducer,[]);
 
-  const addBlogPost = () => {
-    setBlogPosts([
-      ...blogPosts,
-      { title: `Blog posts #${blogPosts.length + 1}` }
-    ]);
-  };
+  const addBlogPost = () =>{
+    dispatch({type:"add_blogPost"})
+  }
 
   return (
-    <BlogContext.Provider value={{ data: blogPosts, addBlogPost: addBlogPost }}>
+    <BlogContext.Provider value={{ data: blogPosts,addBlogPost}}>
       {children}
     </BlogContext.Provider>
   );
